958468e9c8637bb5523c7a431b2aba5f67605dbe
nbochejny <nbochejny@gmail.com>
2018-11-27 11:01:01 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653904.java
@@ -0,0 +1,14 @@
+public class Aufgabe4_1810653904 {
+    public static void main (String[]args){
+        for(int anfangBisEnde = 1; anfangBisEnde < 11; anfangBisEnde++){
+            System.out.println("Der Schleifenzähler ist: " + anfangBisEnde);
+        }
+        //int anfangBisEnde als Anfangspunkt, bis er zur letzen kleinen ganzen Zahl vor 11 kommt, postInkrementiert => nach jedem Durchlauf +1
+        int zahl=1;
+        do{
+            System.out.println("Der Schleifenzähler ist: " + zahl);
+            zahl++;
+        }
+        while (zahl<11);
+    }
+}
